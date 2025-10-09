7f00ac85481e30b08585f8813dbd977265482fb6
Pawinkla <kpkampan@gmail.com>
2025-10-09 15:49:16 +0700

    Update 2_🔗_YouTube_Analysis.py

--- pages/2_🔗_YouTube_Analysis.py
+++ pages/2_🔗_YouTube_Analysis.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="YouTube Analysis", page_icon="🔗")
 st.title("📊 YouTube Data Analysis")
-NOTION_URL = "https://light-sock-d82.notion.site/Data-Scraping-YouTube-top-1000-vdo-9ba2c1ae8e7249bea86d2bffd5b05d70?pvs=143"
+NOTION_URL = "https://light-sock-d82.notion.site/Data-Scraping-YouTube-top-1000-vdo-9ba2c1ae8e7249bea86d2bffd5b05d70"
 tab1, tab2 = st.tabs(["🔗 เปิด Notion โดยตรง", "🧩 ฝัง Notion ในหน้านี้"])
 with tab1:
     st.link_button("เปิดใน Notion", NOTION_URL)
